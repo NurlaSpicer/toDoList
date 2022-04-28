a284da38dbdf5e8c60eba17a237e237290b7ddc0
NurlaSpicer <91671750+NurlaSpicer@users.noreply.github.com>
2022-04-28 18:48:59 +0600

    Add files via upload

---
+++ main/model/ProjectStatus.java
@@ -0,0 +1,7 @@
+package kz.iitu.itse1910.sagyndykov.todo.model;
+
+public enum ProjectStatus {
+    NotStarted,
+    Active,
+    Completed
+}
---
+++ main/model/Tasks.java
@@ -0,0 +1,31 @@
+package kz.iitu.itse1910.sagyndykov.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private long id;
+
+    @Column(name = "task_name")
+    private String name;
+
+    @Column(name = "task_description")
+    private String description;
+
+    @Column(name = "project_priority")
+    private int priority;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "task_taskStatus")
+    private TaskStatus status;
+}
